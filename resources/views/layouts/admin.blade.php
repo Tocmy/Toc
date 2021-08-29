fa2c75ec2b33f01fafd220aa40e0ad9dee4f454d
Tocmy <toc88.my@gmail.com>
2021-08-29 17:24:56 +0800

    update blade

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -5,22 +5,34 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>My Page Title</title>
+        @yield('page-header')
         @include('layouts.admin.head')
         <!--font-->
         <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i|Roboto+Mono:300,400,700|Roboto+Slab:300,400,700" rel="stylesheet"/>
     </head>
     <body class="hold-transition sidebar-mini layout-fixed">
+        <div class="wrapper">
          <!--navbar-->
 
          @include('layouts.admin.main-header')
 
         <!--sidebar-->
         @include('layouts.admin.sidebar')
+        <div class="content-wrapper">
+            <div class="content-header">
+            @include('layouts.breadcrumb')
+        </div>
+        <section class="content">
+            @yield('content')
+        </section>
+        </div>
+        <footer class="main-footer">
+            @include('layouts.admin.footer')
+        </footer>
+        </div>
         <!--Optional JavaScript-->
         <!--jQuery first, then Popper.js, then Bootstrap JS-->
-        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+        @include('layouts.admin.script')
+        @yield('script')
     </body>
 </html>
---
+++ resources/views/layouts/admin/footer.blade.php
@@ -0,0 +1,2 @@
+<strong>Copyright &copy; 2014-2021 <a href="">{{  }}</a>.</strong>
+    {{ __('All rights reserved.') }}
