c07ee93ddc68bccbf101505e6c876b7bc1d74beb
Tocmy <toc88.my@gmail.com>
2021-09-01 19:56:15 +0800

    update user blade

---
+++ resources/views/admin/dashs/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.admin')
+
+@section('title')
+{{ __('User Management') }}
+@endsection
+@section('page-header')
+
+@endsection
+@section('content')
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-12">
+            
+        </div>
+    </div>
+</div>
+@endsection
+
+
+@endpush
