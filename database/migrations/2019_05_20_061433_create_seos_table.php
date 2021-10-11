8fc2ffb2a04ec8014215e2ac2e7176ae8a505ea3
Tocmy <toc88.my@gmail.com>
2021-10-11 09:59:17 +0800

    seo

---
+++ database/migrations/2019_05_20_061433_create_seos_table.php
@@ -0,0 +1,72 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMetadatasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('seos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('title');
+			$table->string('description',155);
+			$table->string('keyword');
+            $table->string('referrer');
+            $table->string('meta_image')->default()->nullable();
+            $table->string('canonical')->nullable();
+            //geo
+            $table->string('geo_region')->default()->nullable();
+            $table->string('geo_position')->default()->nullable();
+            $table->string('geo_placename')->default()->nullable();
+            //dublin
+            $table->string('dcterms_format')->default()->nullable();
+            $table->string('dcterms_language')->default()->nullable();
+            $table->string('dcterms_identifier')->default()->nullable();
+            $table->string('dcterms_relation')->default()->nullable();
+            $table->string('dcterms_publisher')->default()->nullable();
+            $table->string('dcterms_coverage')->default()->nullable();
+            $table->string('dcterms_title')->default()->nullable();
+            $table->string('dcterms_subject')->default()->nullable();
+            $table->string('dcterms_contributor')->default()->nullable();
+            $table->string('dcterms_description')->nullable();
+            //FB openGraph
+            $table->string('og_locale')->default()->nullable();
+            $table->string('og_type')->default()->nullable();
+            $table->string('og_url')->default()->nullable();
+            $table->string('og_title')->default()->nullable();
+            $table->string('og_description')->default()->nullable();
+            $table->string('og_image')->default()->nullable();
+            $table->string('og_site_name')->default()->nullable();
+            $table->string('fb_app_id')->default()->unique();
+            //Twitter
+            $table->string('twitter_card')->default()->nullable();
+            $table->string('twitter_site')->default()->nullable();
+            $table->string('twitter_title')->default()->nullable();
+            $table->string('twitter_description')->nullable();
+            $table->string('twitter_image')->default()->nullable();
+            $table->string('twitter_creator')->default()->nullable();
+            //Schema.org
+
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('seos');
+    }
+}
