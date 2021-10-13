8d9b5daea75232446cfcf4213e035dece2a67e79
Tocmy <toc88.my@gmail.com>
2021-10-13 10:32:49 +0800

    quotation table

---
+++ database/migrations/2019_05_19_124020_create_quotations_table.php
@@ -0,0 +1,94 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuotationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quotations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->softDeletes();
+			$table->datetime('quote_send');
+			$table->date('expiry_date');
+			$table->string('description');
+			$table->string('subject');
+            $table->string('term');
+			$table->string('payment_term');
+			$table->string('delivery_term');
+			$table->string('ref', 64)->unique();
+            $table->foreignId('customer_id')->constrained('customers')
+                  ->onUpdate('cascade')
+                  ->onDelete('cascade');
+            $table->foreignId('store_id')->constrained('companies')
+                  ->onUpdate('cascade')
+                  ->onDelete('cascade');
+            $table->foreignId('tax_id')->constrained('taxes')
+                  ->onUpdate('cascade')
+                  ->onDelete('cascade');
+            $table->foreignId('requote_id')->constrained('quotations')
+                  ->onUpdate('cascade')
+                  ->onDelete('cascade');
+            $table->foreignId('currency_id')->constrained('currencies')
+                  ->onUpdate('cascade')
+                  ->onDelete('cascade');
+            $table->foreignId('history_id')->constrained('histories')
+                  ->onUpdate('cascade')
+                  ->onDelete('cascade');
+            $table->foreignId('status_id')->constrained('statuses')
+                  ->onUpdate('cascade')
+                  ->onDelete('cascade');
+
+        });
+
+
+
+        Schema::create('quotation_products', function(Blueprint $table) {
+			$table->bigIncrements('id');
+			$table->timestamps();
+			$table->integer('quantity');
+			$table->decimal('price', 15,4)->default('0.00');
+			$table->decimal('subtotal', 15,4)->default('0.00');
+			$table->string('description');
+			$table->decimal('base_price', 15,4);
+            $table->decimal('total_qty');
+            $table->decimal('total_discount');
+            $table->decimal('total_tax');
+            $table->decimal('total_price');
+            $table->decimal('tax_rate')->nullable();
+            $table->decimal('tax')->nullable();
+            $table->double('discount')->nullable();
+            $table->decimal('shipping_cost')->nullable();
+            $table->double('grand_total');
+            $table->foreignId('quote_id')->constrained('quotations')
+                  ->onUpdate('cascade')
+                  ->onDelete('cascade');
+            $table->foreignId('product_id')->constrained('products')
+                  ->onUpdate('cascade')
+                  ->onDelete('cascade');
+
+		});
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('quotations');
+        Schema::dropIfExists('quotation_products');
+     
+    }
+}
